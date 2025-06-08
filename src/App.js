1c6a3d38836b250314c4eb967aa0d84960787361
mpo.coder <mpokodner@gmail.com>
2025-06-08 13:42:43 -0500

    made set up changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Dictionary from "/.Dictionary";
 
@@ -6,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <header className="App-header">Header</header>
         <main>
           <Dictionary />
         </main>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 
 export default function Dictionary() {
-  return;
-  <div className="Dictionary">Hello from Dictionary</div>;
+  return (
+    <div className="Dictionary">
+      <h1>Dictionary</h1>
+    </div>
+  );
 }
