376c3a8705d73e8734c2761310b180da1bb7c2e6
mpo.coder <mpokodner@gmail.com>
2025-06-08 17:21:37 -0500

    fixed

--- src/Definition.js
+++ src/Definition.js
@@ -1,6 +1,6 @@
-// Definition Component - handles individual definition display
 import React from "react";
-import "./Definition.css";
+// Note: All styles are handled by Dictionary.css - no separate CSS import needed
+
 function Definition({ meaning, index }) {
   const { definition, example, synonyms = [] } = meaning;
 
@@ -9,22 +9,15 @@ function Definition({ meaning, index }) {
       <div className="definition-text">
         {definition || "No definition available"}
       </div>
+
       {example && <div className="definition-example">{example}</div>}
 
-      <div className="definition-image">
-        {/* Placeholder for image - matching wireframe */}
-        <div className="image-placeholder">
-          <div className="diagonal-line-1"></div>
-          <div className="diagonal-line-2"></div>
-        </div>
-      </div>
-
-      {synonyms && synonyms.length > 0 && (
-        <div className="synonyms-section">
-          <span className="similar-label">Similar</span>
+      {synonyms.length > 0 && (
+        <div className="synonyms-container">
+          <span className="synonyms-label">Similar words:</span>
           <div className="synonyms-list">
-            {synonyms.slice(0, 4).map((synonym) => (
-              <span key={synonym} className="synonym-tag">
+            {synonyms.slice(0, 6).map((synonym, idx) => (
+              <span key={idx} className="synonym-tag">
                 {synonym}
               </span>
             ))}
