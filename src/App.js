f7c346ffdb0d6da160c990a7c0d8afee7fb3dffa
mpo.coder <mpokodner@gmail.com>
2025-06-08 14:31:33 -0500

    changed css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Header</header>
         <main>
           <Dictionary />
         </main>
