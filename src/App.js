358fa5c87be187ccdb0c555590b183598795f411
mpo.coder <mpokodner@gmail.com>
2025-06-09 17:05:31 -0500

    7

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
-import logo from "./logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img
-            src={logo}
-            className="App-logo img-fluid"
-            style={{ height: "40px" }}
-            alt="logo"
-          />
-        </header>
+        <header className="App-header"></header>
         <main>
           <Dictionary />
         </main>
