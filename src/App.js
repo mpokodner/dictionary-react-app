ae9487db26f02843226492b73744c9a98bf83569
mpo.coder <mpokodner@gmail.com>
2025-06-10 08:12:57 -0500

    confirmed bootstrap is working

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
-        <div class="alert alert-primary" role="alert">
-          <p style={{ display: "none" }} className="d-block">
-            Bootstrap is now successfully installed 😃
-          </p>
-          <p className="d-none">
-            Bootstrap is not installed if you can see this 😢
-          </p>
-        </div>
         <footer className="text-center">Coded by Michelle Pokodner</footer>
       </div>
     </div>
